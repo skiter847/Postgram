c6ed642d1ab270de35c8fafc58d4f299a276c11d
artem <ivashenco45@gmail.com>
2020-10-27 01:37:40 +0200

    (frontend)[feature] create component UserInfo

---
+++ frontend/src/components/UserInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="user">
+    <div class="user__picture">
+      <img src="@/assets/img/userpic.jpeg" alt="" style="width: 30px; height: 30px; border-radius: 50%">
+    </div>
+    <div class="user__username">{{ getUserData.first_name }}</div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+  name: "UserInfo",
+
+  computed: {
+    ...mapGetters(['getUserData'])
+  },
+}
+</script>
+
+<style scoped>
+.user{
+  display: flex;
+}
+
+.user__picture{
+  margin-right: 5px;
+}
+
+.user__username{
+  font-size: 15px;
+  margin-top: 4%;
+}
+</style>
