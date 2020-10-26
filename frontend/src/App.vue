e674069fd392fe0dc2f015c2b4c21fca91fab549
artem <ivashenco45@gmail.com>
2020-10-26 14:25:23 +0200

    (frontend)[feature] add html and css to header menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="app">
+  <div class="container">
+    <header class="row">
+      <div class="header__menu">
+        <a class="nav-btn">
+          <div class="nav-item"></div>
+          <div class="nav-item"></div>
+          <div class="nav-item"></div>
+        </a>
+        <span>Postgram</span>
+      </div>
+    </header>
   </div>
 </template>
 
@@ -7,18 +17,46 @@
 
 export default {
   name: 'App',
-  components: {
-  }
+  components: {}
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font: 13px/18px Tahoma,sans-serif,Arial,Helvetica;
+  background-color: #e7ebf0;
+}
+
+span{
+  font-size: 21px;
+  padding-left: 25%;
+}
+
+.header__menu {
+  display: flex;
+  flex: 0 0 25%;
+  padding: 17px;
+  align-items: center;
+}
+
+.header__menu:hover{
+  background-color: #497799;
+}
+
+.nav-btn:hover{
+  cursor: pointer;
+}
+
+.nav-item{
+    width: 18px;
+    height: 2px;
+    background-color: white;
+    margin: 4px 0;
+}
+
+header {
+  background-color: #5580a3;
+  color: white;
 }
 </style>
