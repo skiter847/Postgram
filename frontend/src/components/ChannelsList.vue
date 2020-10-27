3cb42bb4defc24d1ea8180f11daeae94b5a7f7c3
artem <ivashenco45@gmail.com>
2020-10-27 12:24:51 +0200

    (frontend)[feature] create component ChannelsList and now in content component user is not auth show message about this

---
+++ frontend/src/components/ChannelsList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="items">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChannelsList"
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -1,17 +1,53 @@
 <template>
-    <div class="content">
+  <div class="content">
+    <div class="channels__block col-3">
+      <ChannelsList v-if="userIsAuthenticated"/>
+      <div v-else  class="info__authorization">
+        Чтобы начать роботу <span @click="switchLoginModalStatus">Войдите</span> в систему.
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import ChannelsList from "@/components/ChannelsList";
+import {mapGetters, mapMutations} from 'vuex';
+
 export default {
-name: "Content"
+  name: "Content",
+  components: {
+    ChannelsList
+  },
+
+  computed: {
+    ...mapGetters(['userIsAuthenticated'])
+  },
+
+  methods: {
+    ...mapMutations(['switchLoginModalStatus'])
+  },
 }
 </script>
 
 <style scoped>
-.content{
+.content {
   height: 90vh;
   background-color: white;
+}
+
+span{
+  color: royalblue;
+  margin: 0 2px;
+}
+
+.info__authorization{
+  padding-top: 15px;
+  display: flex;
+  flex: 0 0 25%;
+}
+
+.channels__block {
+  border-right: 2px solid rgba(0,0,0,.075);
+  height: 100%;
 }
 </style>
