f0b5abd6c712bc8518c5e915551341545162ffce
artem <ivashenco45@gmail.com>
2020-10-27 12:53:38 +0200

    (frontend)[feature] create button to add channel

--- frontend/src/components/ChannelsList.vue
+++ frontend/src/components/ChannelsList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="items">
-
+  <div class="add-channel">
+    <span>Добавить канал</span>
   </div>
 </template>
 
@@ -12,5 +12,24 @@ export default {
 
 <style scoped>
 
+.add-channel {
+  padding-top: 10px;
+  display: flex;
+  flex: 0 0 25%;
+  justify-content: center;
+}
+
+.add-channel span {
+  font-size: 15px;
+  padding: 10px 25px 10px 25px;
+  color: white;
+  font-weight: bold;
+  background-color: #5580a3;
+  cursor: pointer;
+}
+
+.add-channel span:hover{
+  background-color: #497799;
+}
 
 </style>
