4cdb2ec4223559d0dd4f8f2b964cd638bb59c0dc
artem <ivashenco45@gmail.com>
2020-10-28 16:31:27 +0200

    (backend)[feature] threw path to urls to bot app

--- Postgram/urls.py
+++ Postgram/urls.py
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('cbbf15d8-0421-4512-84d9-5e5d977e3aef/', include('bot.urls')),
 ]
